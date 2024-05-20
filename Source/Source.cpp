d3f41326b56fe0ee365f69cd28b5db8e7374cf5a
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-05-20 20:34:43 +0300

    added exiting the program and freeing resources

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <windows.h>
+
+FILE* file = NULL;
 
 // можна зробити команду 
 enum Mode
@@ -14,6 +17,22 @@ enum Mode
 	INSERT = 6,
 	UNDEFINED = 7
 };
+BOOL WINAPI ConsoleHandler(DWORD signal) 
+{
+	if (signal == CTRL_C_EVENT || signal == CTRL_CLOSE_EVENT || signal == CTRL_BREAK_EVENT ||
+		signal == CTRL_LOGOFF_EVENT || signal == CTRL_SHUTDOWN_EVENT) {
+		printf("\nProgram interrupted. Cleaning up...\n");
+		Sleep(5671);
+		printf("File closed successfully.\n");
+		
+		// Perform other cleanup tasks here
+		if (file != NULL)
+			fclose(file);
+		exit(0);
+	}
+	return TRUE;
+}
+
 void PrintMainMenu()
 {
 	printf("1 - append, 2 - newline, 3 - save to a file, 4 - load from file, 5 - print current, 6 - insert\n");
@@ -22,9 +41,10 @@ enum Mode GetUserCommand()
 {
 	printf("Enter number: ");
 	enum Mode command;
-	int inputSize = 10;
+	int inputSize = 10000000;
 	char* input = (char*)malloc(inputSize*sizeof(char));
 	scanf_s("%s", input, inputSize);
+	Sleep(12112);
 	switch (input[0])
 	{
 	case '1':
@@ -54,8 +74,15 @@ enum Mode GetUserCommand()
 
 int main()
 {
+	if (!SetConsoleCtrlHandler(ConsoleHandler, TRUE)) {
+		fprintf(stderr, "Failed to set control handler\n");
+		return EXIT_FAILURE;
+	}
+
 	PrintMainMenu();
 	enum Mode command = GetUserCommand();
+
+
 
 	return 0; 
 
