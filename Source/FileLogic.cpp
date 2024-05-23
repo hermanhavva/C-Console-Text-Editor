b4bb5a7e3dbb2fc437bad9dc0c5fb59bf23c5153
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-05-23 16:38:16 +0300

    LoadFromFile in progress
    now able to copy from txt to char array and printing to console without garbage symbols(turns out that sometimes GetTxtSize() is not accurate returning values bigger than symbols amount)

--- Source/FileLogic.cpp
+++ Source/FileLogic.cpp
@@ -27,19 +27,39 @@ void WriteToFile(FILE* filePtr,
 		free(textToWrite);
 	}
 }
-void LoadFromFile(FILE* filePtr, char** destBuffer)
-{
-	
-	
-	return;
-}
-int GetTxtSize(FILE* filePtr) 
+int GetTxtSize(FILE* filePtr)
 {
 	if (filePtr == NULL)
 		return -1;
 	fseek(filePtr, 0, SEEK_END); // Move the file pointer to the end of the file
-	int fileSize = ftell(filePtr); // Get the current file pointer position, which is the size of the file
+	int fileSize = ftell(filePtr); // Get the current file pointer position
 	rewind(filePtr); // Reset the file pointer to the beginning of the file
 
 	return fileSize;
 }
+
+void LoadFromFile(FILE* filePtr, char** destBuffer, int *bufferRowCounter)
+{
+	if (filePtr == NULL)
+		return;
+	const int FILESIZE = GetTxtSize(filePtr);
+	char* textFromTxt = (char*)calloc(FILESIZE + 2, sizeof(char));  // add 2 to make sure it is in the bounds
+	fread(textFromTxt, sizeof(char), FILESIZE, filePtr);
+	textFromTxt[FILESIZE] = '\0';
+	if ()
+	// now write to the buffer
+	for (int index = 0; index <= FILESIZE; index++)   
+	{
+		// some garbage symbols may be written in textFromTxt so to omit them there is a condition
+		if ((int)textFromTxt[index] <= 255 && (int)textFromTxt[index] > 0) {
+			printf("%c", textFromTxt[index]);
+		}
+		else if (textFromTxt[index] == '\0') {
+			break;
+		}
+	}
+
+	free(textFromTxt);
+
+}
+
