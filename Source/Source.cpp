347bb921ae511fe659d4fb232877aa2b09107a90
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-05-24 16:30:37 +0300

    added new commands, cls logic, yellow console
    USEREXIT enum added is what to do on the case of exiting the program

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -11,16 +11,19 @@ const int BUFFERSIZE = 256;
 const int COMMANDSIZE = 10;
 char** buffer = NULL;
 int bufferRowCounter = -1;  // default value is -1 empty buffer(no rows)
+HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
 
 enum Mode
 {
+	USEREXIT = 0,
 	APPEND = 1,
 	NEWLINE = 2,
 	SAVETOFILE = 3,
 	LOADFROMFILE = 4,
 	PRINTCURRENT = 5,
 	INSERT = 6,
-	UNDEFINED = 7
+	CLS = 7,
+	UNDEFINED = 8
 };
 BOOL WINAPI ConsoleHandler(DWORD signal) 
 {
@@ -49,14 +52,24 @@ void ExecuteCommand(enum Mode command)
 {
 	errno_t err;  // to track the execution of fopen_s()
 	char* input = (char*)malloc(sizeof(char) * ROWSIZE);
+	if (input == NULL)
+		AllocFailureProgTermination();
+
 	switch (command)
 	{
+	case USEREXIT:
+		printf(">>exiting\n");
+		CloseFile(filePtr);
+		FreeBuffer(buffer,BUFFERSIZE, ROWSIZE, &bufferRowCounter);
+		free(input);
+		Sleep(100);
+		exit(0);
+		break;
+
 	case APPEND:
-		if (input == NULL)
-			AllocFailureProgTermination();
-		
 		fgets(input, ROWSIZE, stdin);
 		strcat_s(buffer[bufferRowCounter], ROWSIZE, input);
+		printf(">>success\n");
 		break;
 	
 	case NEWLINE:
@@ -69,6 +82,7 @@ void ExecuteCommand(enum Mode command)
 					buffer[bufferRowCounter][index] = '\n';
 					buffer[bufferRowCounter][index + 1] = '\0';
 					AddRow(&buffer, BUFFERSIZE, &bufferRowCounter, ROWSIZE);
+					printf(">>success\n");
 					break;
 				}
 			}
@@ -94,6 +108,7 @@ void ExecuteCommand(enum Mode command)
 		}
 		WriteToFile(filePtr, buffer, BUFFERSIZE, ROWSIZE, bufferRowCounter);
 		CloseFile(filePtr); 
+		printf(">>success\n");
 		break;
 
 	case LOADFROMFILE:
@@ -118,14 +133,25 @@ void ExecuteCommand(enum Mode command)
 			break;
 		}
 		LoadFromFile(filePtr, buffer, &bufferRowCounter, BUFFERSIZE, ROWSIZE);
+		printf(">>success");
 		break;
 
 	case PRINTCURRENT:
+		printf("Current text: \n________________________________\n");
+		for (int row = 0; row <= bufferRowCounter; row++) {
+			printf("%s\n", buffer[row]);
+		}
+		printf("________________________________\n");
 		break;
+
 	case INSERT:
 		break;
 	case UNDEFINED:
 		break;
+	case CLS:
+		system("cls");
+		break;
+
 	default:
 		break;
 	}
@@ -137,7 +163,7 @@ void ExecuteCommand(enum Mode command)
 void PrintMainMenu()
 {
 	int curLength = GetRowRemainLength(buffer, bufferRowCounter, ROWSIZE);
-	printf("Row size is %d symbols\nEnter your command:\n1 - append, 2 - newline, 3 - save to a file, 4 - load from file, 5 - print current, 6 - insert\n", curLength);
+	printf("Row space left is %d symbols\nEnter a digit (your command):\n0 - exit, 1 - append, 2 - newline, 3 - save to a file, 4 - load from file, 5 - print current,\n6 - insert, 7 - clean screen\n", curLength);
 }
 enum Mode GetUserCommand() 
 {
@@ -148,6 +174,9 @@ enum Mode GetUserCommand()
 	fgets(input, COMMANDSIZE, stdin);
 	switch (input[0])
 	{
+	case '0':
+		command = USEREXIT;
+		break;
 	case '1':
 		command = APPEND;
 		break;
@@ -162,6 +191,9 @@ enum Mode GetUserCommand()
 		break;
 	case '5':
 		command = PRINTCURRENT;
+		break;
+	case '7':
+		command = CLS;
 		break;
 	default:  // here we need to check for exit
 		printf("The command is not implemmented");
@@ -178,24 +210,25 @@ int main()
 		fprintf(stderr, "Failed to set control handler\n");
 		return EXIT_FAILURE;
 	}
-	
+	SetConsoleTextAttribute(hout, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
+	system("cls");
+
 	InitializeBuffer(&buffer, BUFFERSIZE);
 	AddRow(&buffer, BUFFERSIZE, &bufferRowCounter, ROWSIZE);
 	for (int i = 0; i < 4; i++) {
 		PrintMainMenu();
 		enum Mode command = GetUserCommand();
 		ExecuteCommand(command);
-		printf("%s\n", buffer[bufferRowCounter - 1]);
 
 	}
 	
 
 //	printf("%s", buffer[bufferRowCounter]);
 
-	fopen_s(&filePtr, "hello1.txt", "r");
+	//fopen_s(&filePtr, "hello1.txt", "r");
 	
-	LoadFromFile(filePtr, buffer, &bufferRowCounter, BUFFERSIZE,ROWSIZE);
-	printf("%s", buffer[0]);
+	//LoadFromFile(filePtr, buffer, &bufferRowCounter, BUFFERSIZE,ROWSIZE);
+//	printf("%s", buffer[0]);
 	FreeBuffer(buffer, BUFFERSIZE, ROWSIZE, &bufferRowCounter);
 	CloseFile(filePtr);
 
