fde27a4db933a275cddf9928ee369c84c207b0ff
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-05-26 17:59:01 +0300

    Refactored
    Each command now corresponds to a to Handle functions which are basically the encapsulation of what switch cases in ExecuteCommand() included
    Added:
    void HandleUserExit(char*);
    int HandleAppend(char*);
    int HandleNewLine();
    int HandleSaveToFile(char*);
    int HandleLoadFromFile(char*);
    void HandlePrintCurrent();
    int HandleInsert();

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include "FileLogic.cpp"
 
+
 FILE* filePtr = NULL;
 const int ROWSIZE = 150;
 const int BUFFERSIZE = 256;
@@ -10,6 +11,16 @@ const int COMMANDSIZE = 10;
 char** buffer = NULL;
 int bufferRowCounter = -1;  // default value is -1 empty buffer (no rows)
 HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void HandleUserExit(char*);
+int HandleAppend(char*);
+int HandleNewLine();
+int HandleSaveToFile(char*);
+int HandleLoadFromFile(char*);
+void HandlePrintCurrent();
+int HandleInsert();
+
+
 
 enum Mode
 {
@@ -60,86 +71,9 @@ int RemoveEndNewLine(char string[])  // if last ch is '\n', removes it
 }
 
 
-int HandleInsert()
-{
-	unsigned int row, column;  // might make unsigned
-	char* input = (char*)malloc(sizeof(char) * (ROWSIZE - 1));
-	printf("\nRow for insertion: ");
-	scanf_s(" %u", &row);
-	printf("\nColumn for insertion: ");
-	scanf_s(" %u", &column);
-	fgets(input, ROWSIZE, stdin);  // just to get '\n' out of stdin buffer
-
-	printf("String to insert: ");
-	fgets(input, ROWSIZE, stdin);
-	RemoveEndNewLine(input);  // for omitting '\n'
-
-	if (row > bufferRowCounter || column > ROWSIZE) {
-		printf(">>Row/Column index too big (might use newline first)\n");
-		return -1;
-	}
-	int rowTextLength = strlen(buffer[row]);  // on this index there is '\0'
-	int insertTextLength = strlen(input);
-	int curRowMaxSize = ROWSIZE;  // size is dynamic 
-
-	if ((insertTextLength + rowTextLength + 2) - curRowMaxSize > 0 && (insertTextLength + rowTextLength + 2) - curRowMaxSize < 30)
-	{   // the logic can handle +30 expansion, but not more
-		buffer[row] = (char*)realloc(buffer[row], sizeof(char) * (curRowMaxSize + 30));
-
-		if (buffer[row] == NULL)
-			AllocFailureProgTermination();
-		curRowMaxSize += 30;
-	}
-	else if ((insertTextLength + rowTextLength + 2) - curRowMaxSize >= 30)
-	{
-		printf(">>The row is full or message too big to insert\n");
-		return -1;
-	}
-
-	//if ((insertLength + rowLength + 2) < ROWSIZE)  // + 2 is for '\n' and '\0'
-	//{							 
-	if ((rowTextLength + 1) < column)  // add spaces
-	{							
-		for (int colIndex = rowTextLength - 1; colIndex < column - 1; colIndex++)  // -1 for to handle '\n'
-			buffer[row][colIndex] = ' ';
-
-		buffer[row][column - 1] = '\0';
-		strcat_s(buffer[row], curRowMaxSize - 1, input);
-
-		int curLength = strlen(buffer[row]);
-		buffer[row][curLength] = '\n';  // handle next row 
-		buffer[row][curLength + 1] = '\0';
-	}
-	else if ((rowTextLength + 1) > column)
-	{
-		char* addBuffer = (char*)malloc(sizeof(char) * curRowMaxSize - 1);
-		addBuffer[0] = '\0';
-		char ch = '0';
-		for (int colIndex = column; colIndex < rowTextLength; colIndex++)  // here is the problem
-		{
-			ch = buffer[row][colIndex];
-			strncat_s(addBuffer, curRowMaxSize - 1, &ch, 1);  // one symbol at a time
-				
-		}
-		buffer[row][column] = '\0';
-		//printf("%s", addBuffer);  
-		strcat_s(buffer[row], curRowMaxSize - 1, input);
-		strcat_s(buffer[row], curRowMaxSize - 1, addBuffer);  // buffer too small if realloc
-		free(addBuffer);
-	}
-	else  // only strcat
-	{
-		strcat_s(buffer[row], curRowMaxSize - 1, input);
-	}
-
-	//	}
-	free(input);
-	return 0;
-}
 
 void ExecuteCommand(enum Mode command) 
 {
-	errno_t err;  // to track the execution of fopen_s()
 	char* input = (char*)malloc(sizeof(char) * ROWSIZE);
 	if (input == NULL)
 		AllocFailureProgTermination();
@@ -147,109 +81,40 @@ void ExecuteCommand(enum Mode command)
 	switch (command)
 	{
 	case USEREXIT:
-		printf(">>exiting\n");
-		CloseFile(filePtr);
-		FreeBuffer(buffer,BUFFERSIZE, ROWSIZE, &bufferRowCounter);
-		free(input);
-		Sleep(100);
-		exit(0);
+		HandleUserExit(input);
 		break;
 
 	case APPEND:
-		fgets(input, ROWSIZE, stdin);
-		RemoveEndNewLine(input);  // removing '\n'
-
-		if (GetRowRemainLength(buffer, bufferRowCounter, ROWSIZE) > strlen(input)) {
-			strcat_s(buffer[bufferRowCounter], ROWSIZE - 1, input);  // ROWSIZE-1 for keeping place for '\n'
-			printf(">>success\n");
-		}
-		else
-			printf(">>Error, buffer too small, start a newline\n");
-		
+		HandleAppend(input);
 		break;
 	
 	case NEWLINE:
-		if (bufferRowCounter < BUFFERSIZE - 1)
-		{
-			for (int index = 0; index < ROWSIZE - 1; index++) 
-			{
-				if (buffer[bufferRowCounter][index] == '\0') 
-				{
-					buffer[bufferRowCounter][index] = '\n';
-					buffer[bufferRowCounter][index + 1] = '\0';
-					AddRow(&buffer, BUFFERSIZE, &bufferRowCounter, ROWSIZE);
-					printf(">>success\n");
-					break;
-				}
-			}
-		}
-		else
-			printf("Unable to start a new line(buffer is full)");
+		HandleNewLine();
 		break;
 	
 	case SAVETOFILE:  // ADD in case if user cancels the action
-		printf("\nEnter the filename: ");
-		fgets(input, ROWSIZE, stdin);
-		RemoveEndNewLine(input);  // removing '\n'
-
-		err = fopen_s(&filePtr, input, "a+");
-		if (err != 0 || filePtr == NULL)  // returns 0 if successful
-		{
-			printf("\nCould not open the file");
-			break;
-		}
-		WriteToFile(filePtr, buffer, BUFFERSIZE, ROWSIZE, bufferRowCounter);
-		CloseFile(filePtr); 
-		printf(">>success\n");
+		HandleSaveToFile(input);
 		break;
 
 	case LOADFROMFILE:
-		
-		printf("\nCurrent text will be deleted, 1 - continue, 0 - cancel:\n");
-		fgets(input, ROWSIZE, stdin);
-		if (input[0] == '0')
-			break;
-
-		printf("\nEnter the filename: ");
-		fgets(input, ROWSIZE, stdin);   // remove '\n'
-		RemoveEndNewLine(input);
-
-		err = fopen_s(&filePtr, input, "r");
-		if (err != 0 || filePtr == NULL)  // returns 0 if successful
-		{
-			printf("\nCould not open the file");
-			break;
-		}
-		switch (LoadFromFile(filePtr, buffer, &bufferRowCounter, BUFFERSIZE, ROWSIZE))
-		{
-		case 0:
-			printf(">>success\n");
-			break;
-		case -1:
-			printf(">>failure\n");
-			break;
-		}
+		HandleLoadFromFile(input);
 		break;
 
 	case PRINTCURRENT:
-		printf("Current text: \n________________________________\n");
-		for (int row = 0; row <= bufferRowCounter; row++) {
-			printf("%s", buffer[row]);
-		}
-		printf("\n________________________________\n");
+		HandlePrintCurrent();
 		break;
 
 	case INSERT:
 		HandleInsert();
 		break;
+
 	case UNDEFINED:
 		break;
+
 	case CLS:
 		system("cls");
 		break;
 
-	default:
-		break;
 	}
 	free(input);
 }
@@ -294,7 +159,7 @@ enum Mode GetUserCommand()
 		command = CLS;
 		break;
 	default:  // here we need to check for exit
-		printf("The command is not implemmented");
+		printf(">>The command is not implemmented\n");
 		command = UNDEFINED;
 		break;
 	}
@@ -325,13 +190,191 @@ int main()
 
 
 	Sleep(100);
-	
-	//free(row);
 
 	return 0; 
 
 }
 
 
+
+void HandleUserExit(char* input)
+{
+	printf(">>exiting\n");
+	CloseFile(filePtr);
+	FreeBuffer(buffer, BUFFERSIZE, ROWSIZE, &bufferRowCounter);
+	free(input);
+	Sleep(100);
+	exit(0);
+}
+int HandleAppend(char* input)
+{
+	fgets(input, ROWSIZE, stdin);
+	RemoveEndNewLine(input);  // removing '\n'
+
+	if (GetRowRemainLength(buffer, bufferRowCounter, ROWSIZE) > strlen(input)) {
+		strcat_s(buffer[bufferRowCounter], ROWSIZE - 1, input);  // ROWSIZE-1 for keeping place for '\n'
+		printf(">>success\n");
+		return 0;
+	}
+	else
+	{
+		printf(">>Error, buffer too small, start a newline\n");
+		return -1;
+	}
+}
+int HandleNewLine()
+{
+	if (bufferRowCounter < BUFFERSIZE - 1)
+	{
+		for (int index = 0; index < ROWSIZE - 1; index++)
+		{
+			if (buffer[bufferRowCounter][index] == '\0')
+			{
+				buffer[bufferRowCounter][index] = '\n';
+				buffer[bufferRowCounter][index + 1] = '\0';
+				AddRow(&buffer, BUFFERSIZE, &bufferRowCounter, ROWSIZE);
+				printf(">>success\n");
+				return 0;
+			}
+		}
+	}
+	else
+		printf("Unable to start a new line(buffer is full)");
+
+	return -1;
+}
+int HandleSaveToFile(char* input)
+{
+	errno_t err;  // to track the execution of fopen_s()
+	printf("\nEnter the filename: ");
+	fgets(input, ROWSIZE, stdin);
+	RemoveEndNewLine(input);  // removing '\n'
+
+	err = fopen_s(&filePtr, input, "a+");
+	if (err != 0 || filePtr == NULL)  // returns 0 if successful
+	{
+		printf("\nCould not open the file");
+		return -1;
+	}
+	WriteToFile(filePtr, buffer, BUFFERSIZE, ROWSIZE, bufferRowCounter);
+	CloseFile(filePtr);
+	printf(">>success\n");
+	return 0;
+
+}
+int HandleLoadFromFile(char* input)
+{
+	errno_t err;  // to track the execution of fopen_s() 
+	printf("\nCurrent text will be deleted, 1 - continue, 0 - cancel:\n");
+
+	fgets(input, ROWSIZE, stdin);
+	if (input[0] == '0')
+		return -1;
+
+	printf("\nEnter the filename: ");
+	fgets(input, ROWSIZE, stdin);   // remove '\n'
+	RemoveEndNewLine(input);
+
+	err = fopen_s(&filePtr, input, "r");
+	if (err != 0 || filePtr == NULL)  // returns 0 if successful
+	{
+		printf("\nCould not open the file");
+		return -1;
+	}
+	switch (LoadFromFile(filePtr, buffer, &bufferRowCounter, BUFFERSIZE, ROWSIZE))
+	{
+	case 0:
+		printf(">>success\n");
+		return 0;
+	case -1:
+		printf(">>failure\n");
+		return -1;
+	}
+
+}
+void HandlePrintCurrent() 
+{
+	printf("Current text: \n________________________________\n");
+
+	for (int row = 0; row <= bufferRowCounter; row++)
+		printf("%s", buffer[row]);
+
+	printf("\n________________________________\n");
+}
+int HandleInsert()
+{
+	unsigned int row, column;  // might make unsigned
+	char* input = (char*)malloc(sizeof(char) * (ROWSIZE - 1));
+	printf("\nRow for insertion: ");
+	scanf_s(" %u", &row);
+	printf("\nColumn for insertion: ");
+	scanf_s(" %u", &column);
+	fgets(input, ROWSIZE, stdin);  // just to get '\n' out of stdin buffer
+
+	printf("String to insert: ");
+	fgets(input, ROWSIZE, stdin);
+	RemoveEndNewLine(input);  // for omitting '\n'
+
+	if (row > bufferRowCounter || column > ROWSIZE) {
+		printf(">>Row/Column index too big (might use newline first)\n");
+		return -1;
+	}
+	int rowTextLength = strlen(buffer[row]);  // on this index there is '\0'
+	int insertTextLength = strlen(input);
+	int curRowMaxSize = ROWSIZE;  // size is dynamic 
+
+	if ((insertTextLength + rowTextLength + 2) - curRowMaxSize > 0 && (insertTextLength + rowTextLength + 2) - curRowMaxSize < 30)
+	{   // the logic can handle +30 expansion, but not more
+		buffer[row] = (char*)realloc(buffer[row], sizeof(char) * (curRowMaxSize + 30));
+
+		if (buffer[row] == NULL)
+			AllocFailureProgTermination();
+		curRowMaxSize += 30;
+	}
+	else if ((insertTextLength + rowTextLength + 2) - curRowMaxSize >= 30)
+	{
+		printf(">>The row is full or message too big to insert\n");
+		return -1;
+	}
+
+	if ((rowTextLength + 1) < column)  // add spaces
+	{
+		for (int colIndex = rowTextLength - 1; colIndex < column - 1; colIndex++)  // -1 for to handle '\n'
+			buffer[row][colIndex] = ' ';
+
+		buffer[row][column - 1] = '\0';
+		strcat_s(buffer[row], curRowMaxSize - 1, input);
+
+		int curLength = strlen(buffer[row]);
+
+		buffer[row][curLength] = '\n';  // handle next row 
+		buffer[row][curLength + 1] = '\0';
+	}
+	else if ((rowTextLength + 1) > column)
+	{
+		char* addBuffer = (char*)malloc(sizeof(char) * curRowMaxSize - 1);
+		addBuffer[0] = '\0';
+		char ch = '0';
+		for (int colIndex = column; colIndex < rowTextLength; colIndex++)  // here is the problem
+		{
+			ch = buffer[row][colIndex];
+			strncat_s(addBuffer, curRowMaxSize - 1, &ch, 1);  // one symbol at a time
+
+		}
+		buffer[row][column] = '\0';
+ 
+		strcat_s(buffer[row], curRowMaxSize - 1, input);
+		strcat_s(buffer[row], curRowMaxSize - 1, addBuffer);  // buffer too small if realloc
+
+		free(addBuffer);
+	}
+	else  // only strcat
+	{
+		strcat_s(buffer[row], curRowMaxSize - 1, input);
+	}
+
+	free(input);
+	return 0;
+}
 
 
